dacfb7e1f17dc2ee20a72269c4bbbe0bab06b250
sam5558 <mourabbi.salim@gmail.com>
2020-04-12 14:07:08 +0100

    Delete config.py

--- AffinityScraperMAL/spiders/config.py
+++
@@ -1,4 +0,0 @@
-user_name = 'Guts__'
-password = 's@l1mmal666'
-rankedaff = [('NixoKnight', '68.70'), ('Zlio', '68.30'), ('Bat9Orrin', '67.60'), ('YaBoiBan', '67.00'), ('Domino1175', '63.50'), ('ReyMed', '61.60'), ('EmperorKaido', '61.30'), ('Swesve', '61.20'), ('Zidanenvy', '61.10'), ('Sukaya', '61.00')]
-rankedaffmanga = [('S-cryed', '86.10'), ('-asphyxia-', '79.80'), ('greengolem', '70.00'), ('JojoYabuki', '64.20'), ('TuyNOM', '64.20'), ('BriceMAL', '61.10'), ('_EnmAi_', '60.80'), ('tristan-h', '59.40'), ('Vizioz', '59.10'), ('BenjaminJunior', '59.10')]
